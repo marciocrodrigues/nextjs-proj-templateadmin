ca77cf0fdc441bb749c23441e99dad2d041ce151
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-06-22 00:57:55 -0300

    icones, componente de logo e itens do menu

---
+++ src/components/template/MenuItem.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface MenuItemProps {
+  texto: string;
+  icone: ReactNode;
+  url?: string;
+  className?: string;
+  onClick?: (event: any) => void;
+}
+
+export default function MenuItem(props: MenuItemProps) {
+  function renderizarConteudo() {
+    return (
+      <span
+        className={`
+            flex flex-col justify-center items-center
+            h-20 w-20 text-gray-600 ${props.className}
+        `}
+      >
+        {props.icone}
+        <span
+          className={`
+            text-xs font-light
+          `}
+        >
+          {props.texto}
+        </span>
+      </span>
+    );
+  }
+  return (
+    <li
+      onClick={props.onClick}
+      className={`
+        hover:bg-gray-100
+        cursor-pointer
+        `}
+    >
+      {props.url ? (
+        <Link href={props.url}>{renderizarConteudo()}</Link>
+      ) : (
+        renderizarConteudo()
+      )}
+    </li>
+  );
+}
--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -8,14 +8,15 @@ export default function Titulo(props: TituloProps) {
     <div>
       <h1
         className={`
-
+          font-black text-3xl
+          text-gray-900
       `}
       >
         {props.titulo}
       </h1>
       <h2
         className={`
-      
+          font-light text-sm text-gray-600
       `}
       >
         {props.subtitulo}
