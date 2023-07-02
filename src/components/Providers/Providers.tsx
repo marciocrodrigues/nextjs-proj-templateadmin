0dc2fdf338b4856a3347b8f9fd01c103d1a74533
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-07-01 23:41:49 -0300

    firebasem, autenticacao com google componente avatar

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { AppProvider } from "@/data/context/AppContext";
+import { AuthProvider } from "@/data/context/AuthContext";
 import { ThemeProvider } from "next-themes";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <AppProvider>{children}</AppProvider>
+      <AuthProvider>
+        <AppProvider>{children}</AppProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 };
---
+++ src/pages/perfil.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/template/Layout";
+import useAppData from "@/data/hook/useAppData";
+
+export default function Perfil() {
+  return (
+    <div>
+      <Layout
+        titulo="Perfil do Usuário"
+        subtitulo="Administre as suas informações de usuário!"
+      >
+        <h1>Perfil do Usuário</h1>
+      </Layout>
+    </div>
+  );
+}
